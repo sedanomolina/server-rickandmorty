c27c5ad644cb6930798befc41b17c1e4bc0407b7
raulsedano2410 <sedanomolinaraul@gmail.com>
2023-08-11 19:38:32 -0500

    deploy render

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -17,6 +17,11 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 const sequelize = new Sequelize(DB_DEPLOY, {
     logging: false,
     native: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+        },
+    },
 });
 
 // EJERCICIO 05
